df13335cae202069b134af7c76b12b4df1b78ec8
Arpit Rath <93031760+Hackmey@users.noreply.github.com>
2025-06-08 14:25:51 +0530

    Add files via upload

---
+++ backend/src/main/java/com/arpit/Ushort/UshortApplication.java
@@ -0,0 +1,13 @@
+package com.arpit.Ushort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UshortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UshortApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/arpit/Ushort/UshortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arpit.Ushort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UshortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
